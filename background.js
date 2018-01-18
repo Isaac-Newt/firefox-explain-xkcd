aae6bffd15df2912e7771cc90d51b20ea0d5857e
isaac-newt <isaac.newt.list@icloud.com>
2018-01-17 22:17:07 -0600

    updates

--- background.js
+++ background.js
@@ -3,10 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // Get current active tab
-var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-gettingActiveTab.then((tabs) => {
-  console.log("Hello");
-});
+
 
 // Pseudo-code:
 
@@ -18,8 +15,20 @@ if (activeTab.URL[0-15] == https://xkcd.com/) {
   show pageaction button ();
 }
 
-function onPageActionClicked() {
-  var url = get current URL
+*/
+
+function handleClick() {
+  // Get Active Tab URL
+  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+  gettingActiveTab.then((tabs) => {
+    console.log(tabInfo);
+  });
+
+  // Modify URL (xkcd.com -> explainxkcd.com)
+  var URL = tabInfo.URL;
+
+  // Open modified URL (explainxkcd.com)
 }
 
-*/
+// Add event listener
+browser.pageAction.onClicked.addListener(handleClick);
