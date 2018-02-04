b66ebc4d8defbd9bf751b092a63cba0956e8932d
isaac-newt <isaac.newt.list@icloud.com>
2018-02-03 20:55:50 -0600

    some more stuff.  It doesn't do stuff yet though.

--- background.js
+++ background.js
@@ -2,32 +2,36 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Get current active tab
+// Show on xkcd.com domain
+/*var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+gettingActiveTab.then((tabs) => {
+  let tab = tabs[0];
 
+  function urlIsCorrect(url) {
+    if (url.slice(0,15) == "https://xkcd.com" || url.slice(0,19) == "https://www.xkcd.com") {
+      return true;
+    }
+  }
 
-// Pseudo-code:
-
-/*
-
-var activetab = get active tab();
-
-if (activeTab.URL[0-15] == https://xkcd.com/) {
-  show pageaction button ();
-}
-
-*/
+  if (urlIsCorrect(tab.url)) {
+    browser.pageAction.show(tabs[0].id);
+  }
+});*/
 
 function handleClick() {
   // Get Active Tab URL
   var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
   gettingActiveTab.then((tabs) => {
-    console.log(tabInfo);
+    let tab = tabs[0];
+    console.log(tab.url);
+
+    // Modify URL
+    let url = tab.url;
+    console.log(url.slice(17));
+
+    let newURL = "https://www.explainxkcd.com/" + url.slice(17);
+    console.log(newURL);
   });
-
-  // Modify URL (xkcd.com -> explainxkcd.com)
-  var URL = tabInfo.URL;
-
-  // Open modified URL (explainxkcd.com)
 }
 
 // Add event listener
