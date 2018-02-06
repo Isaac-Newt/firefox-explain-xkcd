be4380970ade89ebed768aa4684b5583566b37b8
isaac-newt <isaac.newt.list@icloud.com>
2018-02-06 10:11:20 -0600

    don't need to do this here, is now handled in the manifest

--- background.js
+++ background.js
@@ -2,22 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Show on xkcd.com domain
-/*var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-gettingActiveTab.then((tabs) => {
-  let tab = tabs[0];
-
-  function urlIsCorrect(url) {
-    if (url.slice(0,15) == "https://xkcd.com" || url.slice(0,19) == "https://www.xkcd.com") {
-      return true;
-    }
-  }
-
-  if (urlIsCorrect(tab.url)) {
-    browser.pageAction.show(tabs[0].id);
-  }
-});*/
-
 function handleClick() {
   // Get Active Tab URL
   var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
