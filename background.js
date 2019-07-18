9d130c2c5d0090f42a7763b2267ab74529535bc6
Isaac <isaac.newt.list@icloud.com>
2019-07-17 22:09:00 -0500

    Rewrite to use regex rather than string slicing
    This rewrite uses regex to get the comic number rather than slicing the URL string, which makes the code a lot cleaner.  Also cleaned up the code formatting.

--- background.js
+++ background.js
@@ -1,48 +1,31 @@
+/*
+ * Author(s): Isaac List
+ * Modified: 2019
+ */
+
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 function handleClick() {
-  // Get Active Tab URL
-  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-  gettingActiveTab.then((tabs) => {
-    let tab = tabs[0];
-      // Debugging stuff
-      // console.log(tab.url);
-      // console.log(tab.url.slice(0, 11));
+    // Get Active Tab URL
+    var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
 
     // Modify the URL
+    gettingActiveTab.then((tabs) => {
+        let tab = tabs[0];
+        let url_regex = /\d+/;
+        var comic_number = "";
 
-    // For www domain
-    if (tab.url.slice(0, 11) == "https://www") {
-      let url = tab.url;
-        // Debugging stuff
-        // console.log(url.slice(0, 11));
-        // console.log(url.slice(20));
+        if (tab.url.match(url_regex)) {
+            var comic_number = tab.url.match(url_regex)[0];
+        }
 
-      let newURL = "https://explainxkcd.com/" + url.slice(20);
-        // Debugging stuff
-        // console.log(newURL);
+        let newURL = "https://explainxkcd.com/" + comic_number;
         browser.tabs.create({
-          "url": newURL
+            "url": newURL
         });
-    }
-
-    // For normal domain
-    else {
-      let url = tab.url;
-        // Debugging stuff
-        // console.log(url.slice(17));
-
-      let newURL = "https://explainxkcd.com/" + url.slice(17);
-        // Debugging stuff
-        // console.log(newURL);
-      browser.tabs.create({
-        "url": newURL
-      });
-    }
-
-  });
+    });
 }
 
 // Add event listener
